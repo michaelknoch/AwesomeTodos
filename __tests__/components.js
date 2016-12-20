43c8f7226dd74e6c86191c782290a90f6f86005c
Michael Knoch <info@michaelknoch.de>
2016-12-20 16:36:02 +0100

    add headline tests

--- __tests__/components.js
+++ __tests__/components.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+
 import SwipeList from '../app/components/swipeList';
+import { H1, H2, H3 } from '../app/components/headlines';
 
 const todos = [
     { id: 0, text: 'initial' },
@@ -11,6 +13,29 @@ describe('swipeList component', () => {
     it('should render correctly', () => {
         const tree = renderer.create(
             <SwipeList todos={todos} />
+        ).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
+
+describe('headlines', () => {
+    it('should render H1 correctly', () => {
+        const tree = renderer.create(
+            <H1>headline 1</H1>
+        ).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('should render H2 correctly', () => {
+        const tree = renderer.create(
+            <H2>headline 2</H2>
+        ).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('should render H3 correctly', () => {
+        const tree = renderer.create(
+            <H3>headline 3</H3>
         ).toJSON();
         expect(tree).toMatchSnapshot();
     });
